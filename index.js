3eb32ae0b9b3558208102f09cf14debe46c47759
AsibHasanRiyad <asibhasanriyad@gmail.com>
2023-11-06 11:39:41 +0600

    assignmets data get route is added

--- index.js
+++ index.js
@@ -11,7 +11,7 @@ app.use(express.json());
 
 
 
-const uri = "mongodb+srv://learnify:SqPXKPxDyPHGbrvz@cluster0.2x6r8lz.mongodb.net/?retryWrites=true&w=majority";
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.2x6r8lz.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
 const client = new MongoClient(uri, {
@@ -25,13 +25,35 @@ const client = new MongoClient(uri, {
 async function run() {
   try {
     // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
+    // await client.connect();
+
+    const assignmentCollection = client.db('Learnify').collection("allAssignment");
+
+    //receive data from client side
+    app.post('/assignments', async(req, res) =>{
+        const newAssignment = req.body;
+        console.log('data received from client',newAssignment);
+        const result = await assignmentCollection.insertOne(newAssignment);
+        // console.log(result);
+        res.send(result)
+
+    })
+
+    //get data 
+    app.get('/assignments', async(req, res) =>{
+        const cursor = assignmentCollection.find();
+        const result = await cursor.toArray();
+        res.send(result)
+    })
+
+
+
     // Send a ping to confirm a successful connection
     await client.db("admin").command({ ping: 1 });
     console.log("Pinged your deployment. You successfully connected to MongoDB!");
   } finally {
     // Ensures that the client will close when you finish/error
-    await client.close();
+    // await client.close();
   }
 }
 run().catch(console.dir);
