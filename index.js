512e4efe01c630a42636d9c60683c5662e7b9283
AsibHasanRiyad <asibhasanriyad@gmail.com>
2023-11-06 09:27:02 +0600

    mongodb is added

--- index.js
+++ index.js
@@ -1,9 +1,9 @@
-const express = require('express');
-const cors = require('cors');
-require('dotenv').config()
+const express = require("express");
+const cors = require("cors");
+const { MongoClient, ServerApiVersion } = require("mongodb");
+require("dotenv").config();
 const app = express();
 const port = process.env.PORT || 5001;
-
 
 //middleware
 app.use(cors());
@@ -11,11 +11,37 @@ app.use(express.json());
 
 
 
-//
-app.get('/', (req, res) =>{
-    res.send('Learnify server is running')
+const uri = "mongodb+srv://learnify:SqPXKPxDyPHGbrvz@cluster0.2x6r8lz.mongodb.net/?retryWrites=true&w=majority";
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+  serverApi: {
+    version: ServerApiVersion.v1,
+    strict: true,
+    deprecationErrors: true,
+  }
 });
 
-app.listen(port, () =>{
-    console.log(`Learnify server is running on port:${port}`);
-})
+async function run() {
+  try {
+    // Connect the client to the server	(optional starting in v4.7)
+    await client.connect();
+    // Send a ping to confirm a successful connection
+    await client.db("admin").command({ ping: 1 });
+    console.log("Pinged your deployment. You successfully connected to MongoDB!");
+  } finally {
+    // Ensures that the client will close when you finish/error
+    await client.close();
+  }
+}
+run().catch(console.dir);
+
+
+//
+app.get("/", (req, res) => {
+  res.send("Learnify server is running");
+});
+
+app.listen(port, () => {
+  console.log(`Learnify server is running on port:${port}`);
+});
